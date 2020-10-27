442c97bb4ce6faa7a6f9e8c39ae2209ad5489efa
MjShin <killrain@gmail.com>
2020-10-27 19:12:00 +0900

    update publish script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,4 @@
+cd /home/oracle/work/oracle-front
 echo '{"lastUpdated":"' | tr -d '\n' > ./src/Data/LastUpdate.json
 date "+%Y-%m-%d %H:%M:%S" | tr -d '\n' >> ./src/Data/LastUpdate.json
 echo '"}' >> ./src/Data/LastUpdate.json
