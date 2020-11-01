0840ab3bf54b7b0c1bad1745bd228cfc754356a8
MjShin <killrain@gmail.com>
2020-11-02 03:12:28 +0900

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/oracle-front/precache-manifest.7df2b608641888611f00a86339b79e42.js"
+  "/oracle-front/precache-manifest.69c74ff45dbf2e2d3a2bee9f02a143fc.js"
 );
 
 self.addEventListener('message', (event) => {
